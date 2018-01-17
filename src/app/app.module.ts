32b7d1db921327e7735c2a01bc2cf1ddee993e80
João Junceira <joaojunceira@hotmail.com>
2018-01-17 18:07:02 +0000

    Create Issue form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IssueComponent } from './issue/issue.component';
+import { ManagerComponent } from './manager/manager.component';
+import { NotificationComponent } from './notification/notification.component';
+import { BoardComponent } from './board/board.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueComponent,
+    ManagerComponent,
+    NotificationComponent,
+    BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
